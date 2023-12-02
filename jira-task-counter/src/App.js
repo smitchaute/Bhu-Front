dd8ed4b8d7f03054aba186cd84715e6678357c32
Smit <smit@Viniths-MacBook-Pro-15.local>
2023-12-02 16:04:00 +0530

    initial commit

---
+++ jira-task-counter/src/App.js
@@ -0,0 +1,37 @@
+// src/App.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [assignedCount, setAssignedCount] = useState(0);
+  const [unassignedCount, setUnassignedCount] = useState(0);
+
+  useEffect(() => {
+    const fetchTaskCounts = async () => {
+      try {
+        const assignedResponse = await axios.get('http://localhost:3001/assigned-count');
+        setAssignedCount(assignedResponse.data.count);
+
+        const unassignedResponse = await axios.get('http://localhost:3001/unassigned-count');
+        setUnassignedCount(unassignedResponse.data.count);
+      } catch (error) {
+        console.error('Error fetching task counts:', error.message);
+      }
+    };
+
+    fetchTaskCounts();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h1>Jira Task Counter</h1>
+      <div className="task-counts">
+        <p>Assigned Tasks: {assignedCount}</p>
+        <p>Unassigned Tasks: {unassignedCount}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
